4b95928ca5c6728d0600c79ab584c04b4e109111
Eliza Henderson <Eliza Henderson>
2016-10-15 13:21:12 -0700

    lab3 mid point?

---
+++ Lab3/Lab3/WebForms/RequireedTextBox.ascx
@@ -0,0 +1,5 @@
+﻿<%@ Control Language="C#" AutoEventWireup="true" CodeBehind="RequireedTextBox.ascx.cs" Inherits="Lab3.WebForms.RequireedTextBox" %>
+
+<asp:Label runat="server" ID="uxLabel"></asp:Label>
+<asp:TextBox runat="server" ID="uxTextBox"></asp:TextBox>
+<asp:RequiredFieldValidator runat="server"  ID="uxFieldValidator"></asp:RequiredFieldValidator>
---
+++ Lab3/Lab3/WebForms/RequireedTextBox.ascx.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Lab3.WebForms
+{
+    public partial class RequireedTextBox : System.Web.UI.UserControl
+    {
+        public string LabelText
+        {
+            get { return uxLabel.Text; }
+            set { uxLabel.Text = value; }
+        }
+
+        public string TextBoxText
+        {
+            get { return uxTextBox.Text; }
+            set { uxTextBox.Text = value; }
+        }
+
+        public string ValidationGroup
+        {
+            get { return uxFieldValidator.ValidationGroup.ToString(); }
+            set { uxFieldValidator.ValidationGroup = value; }
+        }
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
