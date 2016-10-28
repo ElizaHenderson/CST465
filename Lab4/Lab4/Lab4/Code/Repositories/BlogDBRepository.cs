09382480e36dcbbd6f2834dc56777567175cdbd1
Elizabeth Henderson <elizabeth.henderson@students.cset.oit.edu>
2016-10-27 20:33:32 -0700

    lab5 break - The Views

---
+++ Lab4/Lab4/Lab4/Code/Repositories/BlogDBRepository.cs
@@ -0,0 +1,94 @@
+﻿using Lab4.Code.DataObjects;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+
+namespace Lab4.Code.Repositories
+{
+    public class BlogDBRepository : IDataEntityRepository<BlogPost>
+    {
+        public BlogPost Get(int id)
+        {
+            BlogPost item = new BlogPost();
+            using (SqlConnection connection = new SqlConnection(ConfigurationManager.ConnectionStrings["Aura"].ConnectionString))
+            {
+                using (SqlCommand command = new SqlCommand())
+                {
+                    command.Connection = connection;
+                    command.CommandText = "SELECT * FROM Blog WHERE ID=@ID";
+                    command.Parameters.AddWithValue("@ID", id);
+                    command.Connection.Open();
+                    using (SqlDataReader reader = command.ExecuteReader())
+                    {
+                        if (reader.Read())
+                        {
+                            item.ID = (int)reader["ID"];
+                            item.Title = reader["Title"].ToString();
+                            item.Content = reader["Content"].ToString();
+                            item.Author = reader["Author"].ToString();
+                            item.Timestamp = DateTime.Parse(reader["Timestamp"].ToString());
+                        }
+                    }
+                }
+            }
+            return item;
+        }
+
+        public List<BlogPost> GetList()
+        {
+            List<BlogPost> items = new List<BlogPost>();
+            using (SqlConnection connection = new SqlConnection(ConfigurationManager.ConnectionStrings["Aura"].ConnectionString))
+            {
+                using (SqlCommand command = new SqlCommand())
+                {
+                    command.Connection = connection;
+                    command.CommandText = "SELECT * FROM Blog";
+                    command.Connection.Open();
+                    using (SqlDataReader reader = command.ExecuteReader())
+                    {
+                        while(reader.Read())
+                        {
+                            BlogPost item = new BlogPost();
+                            item.ID = (int)reader["ID"];
+                            item.Title = reader["Title"].ToString();
+                            item.Content = reader["Content"].ToString();
+                            item.Author = reader["Author"].ToString();
+                            item.Timestamp = DateTime.Parse(reader["Timestamp"].ToString());
+                            items.Add(item);
+                        }
+                    }
+                }
+            }
+            return items;
+        }
+
+        public void Save(BlogPost entity)
+        {
+            using (SqlConnection connection = new SqlConnection(ConfigurationManager.ConnectionStrings["Aura"].ConnectionString))
+            {
+                using (SqlCommand command = new SqlCommand())
+                {
+                    command.Connection = connection;
+                    if(entity.ID == 0)
+                    {
+                        command.CommandText = "INSERT INTO Blogs(Author, Title, Content) VALUES(@Title, @Content, @Author)";
+                        command.Parameters.AddWithValue("@Author", entity.Author); 
+                    }
+                    else
+                    {
+                        command.CommandText = "UPDATE Blog SET Title=@Title, Content=@Content WHERE ID=@ID";
+                        command.Parameters.AddWithValue("@ID", entity.ID);
+                    }
+                    command.Parameters.AddWithValue("@Author", entity.Author);
+                    command.Parameters.AddWithValue("@Title", entity.Title);
+                    command.Parameters.AddWithValue("@Content", entity.Content);
+                    command.Connection.Open();
+                    command.ExecuteNonQuery();
+                }
+            }
+        }
+    }
+}
---
+++ Lab4/Lab4/Lab4/Controllers/BlogController.cs
@@ -0,0 +1,74 @@
+﻿using Lab4.Code.DataObjects;
+using Lab4.Code.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Lab4.Controllers
+{
+    public class BlogController : Controller
+    {
+        private IDataEntityRepository<BlogPost> _thing;
+        // GET: Blog
+        public ActionResult Index()
+        {
+            return View(_thing.GetList());
+        }
+
+        public ActionResult Add()
+        {
+            BlogPostModel _obj = new BlogPostModel();
+            return View(_obj);
+        }
+
+        [HttpPost]
+        public ActionResult Add(BlogPostModel model)
+        {
+            if(ModelState.IsValid)
+            {
+                BlogPost post = new BlogPost();
+                post.Author = model.Author;
+                post.Content = model.Content;
+                post.ID = model.ID;
+                post.Title = model.Title;
+                _thing.Save(post);
+            }
+            return RedirectToAction("Index");
+        }
+        public ActionResult Edit(int id)
+        {
+            BlogPost post = new BlogPost();
+            BlogPostModel model = new BlogPostModel();
+            post = _thing.Get(id);
+            model.Author = post.Author;
+            model.Content = post.Content;
+            model.ID = post.ID;
+            model.Title = post.Title;
+            return View(model);
+        }
+
+        [HttpPost]
+        public ActionResult Edit(BlogPostModel model)
+        {
+            if(ModelState.IsValid)
+            {
+                BlogPost post = new BlogPost();
+                post.Author = model.Author;
+                post.Content = model.Content;
+                post.ID = model.ID;
+                post.Title = model.Title;
+                _thing.Save(post);
+            }
+            return RedirectToAction("Index");
+
+        }
+        public BlogController ()
+        {
+            _thing = new BlogDBRepository();
+        }
+        
+    }
+    
+}
