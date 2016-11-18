726dac4ed647a017d1e555403cb1c40db6f655b7
Elizabeth Henderson <elizabeth.henderson@students.cset.oit.edu>
2016-11-17 20:07:43 -0800

    lab6 done

--- Lab4/Lab4/Lab4/Controllers/HomeController.cs
+++ Lab4/Lab4/Lab4/Controllers/HomeController.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Lab4.Code.DataObjects;
+using Lab4.Code.Repositories;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -8,10 +10,20 @@ namespace Lab4.Controllers
 {
     public class HomeController : Controller
     {
+        IDataEntityRepository<BlogPost> _stuff;
+        public HomeController()
+        {
+            _stuff = new BlogDBRepository();
+        }
+        //public HomeController(IDataEntityRepository<BlogPost> thing)
+        //{
+        //    _stuff = thing;
+        //}
         // GET: Home
         public ActionResult Index()
         {
-            return View();
+            List<BlogPost> thingy = _stuff.GetList();
+            return View(thingy);
         }
     }
 }
